f84161e8822dc3f6704ed4f7da7a35a6e47cc8ac
Nicolas Mahnic <nmahnic@pdssa.com.ar>
2022-04-17 13:16:19 -0300

    perform Socket

--- sockets/socket.js
+++ sockets/socket.js
@@ -2,15 +2,9 @@ const { io } = require('../index')
 
 io.on('connection', client => {
     console.log('Cliente conectado')
-//   client.on('event', data => { /* … */ })
+
     client.on('disconnect', () => {
         console.log('Cliente desconectado')
-    })
-
-    client.on('mensaje', (payload) => {
-        console.log(`Mensaje: ${payload.nombre}`)
-
-        client.emit('mensaje', { admin: 'Nuevo mensaje'})
     })
     
     client.on('emitir-mensaje', (payload) => {
